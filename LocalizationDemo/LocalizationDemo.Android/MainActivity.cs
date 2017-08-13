c72a82051ad7152c4872c54429441f68bb288dcb
Bryan Anthony Garcia <bryananthonygarcia@live.com>
2017-08-13 16:28:32 +0800

    Initial

--- LocalizationDemo/LocalizationDemo.Android/MainActivity.cs
+++ LocalizationDemo/LocalizationDemo.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace LocalizationDemo.Droid
 {
-    [Activity(Label = "LocalizationDemo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "@string/app_name", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
