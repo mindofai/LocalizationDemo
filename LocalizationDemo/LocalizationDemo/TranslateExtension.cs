2b76a7a3675484bcb66a2aaeda0d58bdb71aa8fe
Bryan Anthony Garcia <bryananthonygarcia@live.com>
2017-08-13 14:59:30 +0800

    Add project files.

---
+++ LocalizationDemo/LocalizationDemo/TranslateExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalizationDemo
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "LocalizationDemo.Resources.AppResource";
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+            ResourceManager resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            return resourceManager.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
